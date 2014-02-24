0313a59dc79d80aac57404573edcad317c89eb46
Jeff Thomas <jefferyjthomas@gmail.com>
2014-02-24 14:12:58 -0700

    Pong to Thomas. paper_trail gem installed and created migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140219223305) do
+ActiveRecord::Schema.define(version: 20140224210612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,13 +76,12 @@ ActiveRecord::Schema.define(version: 20140219223305) do
     t.datetime "created_at"
   end
 
-  add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true, using: :btree
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
 
   create_table "tags", force: true do |t|
     t.string "name"
   end
-
-  add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",   null: false
@@ -100,9 +99,9 @@ ActiveRecord::Schema.define(version: 20140219223305) do
     t.string   "first_name"
     t.string   "last_name"
     t.boolean  "display_email"
+    t.string   "youtube_id"
     t.string   "slug"
     t.boolean  "display_profile",        default: true
-    t.string   "youtube_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
