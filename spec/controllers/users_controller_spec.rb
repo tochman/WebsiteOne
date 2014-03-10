ff100155a8ee4b8e22e6647fb32d865bfb3ee0b7
Thomas Ochman <thomas@nocebo.se>
2014-03-10 20:05:55 +0100

    added new context to 'users_controller_spec'

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -97,7 +97,61 @@ describe UsersController do
   end
 
   describe 'send hire me button message' do
+    let(:valid_params) { {name: 'Thomas', email: 'thomas@email.com', message: 'Want to hire you!'} }
+    let(:user) { @user }
+    before(:each) do
+      @user = User.new(first_name: 'Marcelo',
+                       last_name: 'Mr G',
+                       email: 'marcelo@whatever.com',
+                       password: '1234567890')
+
+      @previous_page = '/'
+      @request.env['HTTP_REFERER'] = @previous_page
+      Mailer.stub_chain(:hire_me_form, :deliver).and_return(true)
+
+      ActionMailer::Base.delivery_method = :test
+      ActionMailer::Base.perform_deliveries = true
+      ActionMailer::Base.deliveries = []
+
+    end
 
 
+    it 'renders successful message' do
+      post :hire_me_contact_form, valid_params
+      expect(response).to redirect_to @previous_page
+      expect(flash[:notice]).to eq('Your message has been sent successfully!')
+    end
+
+    it 'renders failure message' do
+      Mailer.stub_chain(:hire_me_form, :deliver).and_return(false)
+      post :hire_me_contact_form, valid_params
+      expect(response).to redirect_to @previous_page
+      expect(flash[:alert]).to eq('Your message has not been sent!')
+    end
+
+    it 'checks field Name' do
+      post :hire_me_contact_form, name: '', message: '123'
+      expect(flash[:alert]).to eq('Please, fill in Name and Message field')
+    end
+
+    it 'checks field Message' do
+      post :hire_me_contact_form, name: '123', message: ''
+      expect(flash[:alert]).to eq('Please, fill in Name and Message field')
+    end
+
+    it 'calls mailer' do
+      expect(Mailer).to receive(:hire_me_form) do |arg|
+        expect(arg).to include(valid_params)
+        double(ActionMailer::Base).as_null_object
+      end
+      post :hire_me_contact_form, valid_params
+    end
+
+    it 'sends the email to site admin' do
+      allow(Mailer).to receive(:hire_me_form).and_call_original
+
+      post :hire_me_contact_form, valid_params
+      expect(ActionMailer::Base.deliveries[0].body).to include('Want to hire you!')
+    end
   end
 end
